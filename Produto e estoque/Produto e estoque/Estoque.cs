a65693c8f62eb57f05be6105ef2941d3c3997ac5
Vinicius Mello <153565970+ViniciusFelicianoMello@users.noreply.github.com>
2024-03-04 09:56:24 -0300

    Add files via upload

---
+++ Produto e estoque/Produto e estoque/Estoque.cs
@@ -0,0 +1,24 @@
+﻿class Estoque
+{
+    private List<Produto> Produtos {  get; set; } = new List<Produto>();
+    public void AdicionarProduto(Produto produto)
+    {
+        Produtos.Add(produto);
+        Console.WriteLine($"Produto {produto.Nome} adicionado ao estoque com sucesso");
+    }
+
+    public void ExibirProdutos()
+    {
+        if ( Produtos.Count == 0 )
+        {
+            Console.WriteLine("Estoque está vazio, por favor, adicione produtos ao estoque");
+        } else
+        {
+            Console.WriteLine("Lista de produtos em estoque:");
+            foreach( var produto in Produtos )
+            {
+                Console.WriteLine(produto.Descricao);
+            }
+        }
+    }
+}
---
+++ Produto e estoque/Produto e estoque/Produto.cs
@@ -0,0 +1,11 @@
+﻿class Produto
+{
+    private double Preco;
+    private int Estoque;
+    public string Nome { get; set; }
+    public string Marca { get; set; }
+
+    public string Descricao =>
+        $"{Nome} {Marca} - {Preco} - Estoque: {Estoque}";
+}
+
